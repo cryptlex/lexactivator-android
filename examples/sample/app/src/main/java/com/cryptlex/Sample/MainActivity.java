c86767284dba1fdd2a664575b6e62a9927097ee3
Adnan Kamili <adnan.kamili@gmail.com>
2021-06-15 16:42:41 +0530

    updated example

--- examples/sample/app/src/main/java/com/cryptlex/Sample/MainActivity.java
+++ examples/sample/app/src/main/java/com/cryptlex/Sample/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private void initLexActivator() throws LexActivatorException {
         LexActivator.SetDataDirectory(getApplicationContext().getFilesDir().getAbsolutePath());
         LexActivator.SetAndroidId(Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID));
-        LexActivator.SetProductData("QjVFOUU4ODkyRDZDQ0E5OEUzMTkxRjI1MDU2QTI0NEQ=.QZId4v+XnZXqm8lKHBJi4R3Q2iCYC6L0ZPi6/MZEr7Oj5GeIx3Ze1ii/Zf7vztFYnXwtYLcyY+qu18VZ5IxcxOzrtJZvv21hwocytvxDyJajt9FW6MdmamMNsXVf2XC1gG2xa6pq0qIXiv0VxWsuxAmgKWK0g8DKDRQgJSx3B5yITaeg3u4urFWsF10jZsmFeA/xwMG8vs9/tw9XHgzUbnt5eWZdWhScT+yVoEPx4OnUJ74joD2awHUzCeW3KjGlJOhTM/AIGKE3eeFPHSWcvKBliLcHT7QO2NHYPWs6OJ+K5PMuGurFFafb4S3TpJLLYmx1G4hBxBbmsz0IA/i1OQKO97pmwYYPcYbQV1jOuzksuaFZlvsNtNnRv/QxmJgLR1reXKmJtC29cYI6OErO/sJIpd95Nbwj4Ww4F469qANI3fw0FqUABDvd5VfBLnMJaYyxIuOt4PZslWoE8lfY72HJUIcFuSMao8MXeRMp/aFRPVXca7qcfO66mW5yHHn5rRtsMjx/A/xyC++7LQr6ytY3OIM2GK6nzy0fYlMu6wOY3w5r8YpKnlWVXIXyKC0RW3EgtrIPiPwf5bftr+IDh9Np6l8IQzfd/J2MgluGebgCQK6zMhn+BmwUorixhkrdFEAImLcMYwlaMk0pdiSUCZjireJ5Q+jTRCWSMPPeneBGRpObzcCNVSlDOku9K8v/CikJqybhGd95nZJc7dtvwR/OPnQn6GxaPFNlkRKLAkc=");
-        LexActivator.SetProductId("74a87f2e-1fdf-46e2-a205-4af3357f553b", LexActivator.LA_USER);
+        LexActivator.SetProductData("PASTE_PRODUCT_DATA");
+        LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.LA_USER);
     }
 
     public void activateLicense(View view) {
