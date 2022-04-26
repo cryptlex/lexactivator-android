5032fcb635c2437b56cb0311d8363ffcb6635897
Azan <azan.farooqi@cryptlex.com>
2022-04-26 14:19:36 +0530

    fix(example): add exception handling

--- examples/sample/app/src/main/java/com/cryptlex/Sample/MainActivity.java
+++ examples/sample/app/src/main/java/com/cryptlex/Sample/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (LexActivatorException ex) {
             statusTextView.setText("License activation failed! Error code: " + ex.getCode() + " Error message: " + ex.getMessage());
+        } catch (Exception ex) {
+            statusTextView.setText("License activation failed! Error message: " + ex.getMessage());
         }
     }
 
@@ -55,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (LexActivatorException ex) {
             statusTextView.setText("Error code: " + ex.getCode() + " Error message: " + ex.getMessage());
+        } catch (Exception ex) {
+            statusTextView.setText("Error message: " + ex.getMessage());
         }
     }
 }
